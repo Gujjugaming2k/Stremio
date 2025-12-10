91c59a5549b60217f4aa0b91cd97ce8972c33506
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 11:16:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,20 @@ curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o /tmp/stremio_scripts/backup_runner.py
 nohup python3 /tmp/stremio_scripts/backup_runner.py > /tmp/backup_runner.log 2>&1 &
 nohup node /tmp/Stremio/server.js > server.log 2>&1 &
+
+# Base64-encoded credentials
+ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
+ENCODED_CHANNEL_ID="LTEwMDIxOTY1MDM3MDU="
+
+# Decode at runtime
+BOT_TOKEN=$(echo "$ENCODED_TOKEN" | base64 --decode)
+CHANNEL_ID=$(echo "$ENCODED_CHANNEL_ID" | base64 --decode)
+
+# Message to send
+MESSAGE="VFlixPrime Stremio - Started"
+
+# Send the message using curl
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -d chat_id="${CHANNEL_ID}" \
+    -d text="${MESSAGE}" \
+    -d parse_mode="Markdown"  # or "HTML" for HTML formatting
