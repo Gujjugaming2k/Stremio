17c51afe3651fd1f774687272cceed64dc1c93f7
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 11:42:01 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,13 @@ sudo /etc/init.d/cloudflared start
 sudo /etc/init.d/cloudflared start
 sudo /etc/init.d/cloudflared start
 
-
+sudo chmod 777 *
 mkdir -p stremio_scripts
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o stremio_scripts/backup_to_github.py
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o stremio_scripts/backup_runner.py
+sudo chmod 777 *
+sudo chmod 777 Stremio/*
+sudo chmod 777 /workspaces/FileStreamBot_2024/Stremio/*
 nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
 nohup sudo node Stremio/server.js > server.log 2>&1 &
 
