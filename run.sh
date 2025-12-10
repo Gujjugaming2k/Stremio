9cb86be2d6567aa8bb23a5e5fc349241ea4321ab
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 12:16:18 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,4 +20,26 @@ id >> id.txt
 sleep 20
 nohup node /workspaces/FileStreamBot_2024/Stremio/server.js > /workspaces/FileStreamBot_2024/server.log 2>&1 &
 
+# Base64-encoded credentials
+ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
+ENCODED_CHANNEL_ID="LTEwMDIxOTY1MDM3MDU="
 
+# Decode at runtime
+BOT_TOKEN=$(echo "$ENCODED_TOKEN" | base64 --decode)
+CHANNEL_ID=$(echo "$ENCODED_CHANNEL_ID" | base64 --decode)
+
+# Message to send
+MESSAGE="VFlixPrime Stremio - Started"
+
+# Check if the message was sent successfully
+if [ $? -eq 0 ]; then
+    echo "Message sent successfully!"
+else
+    echo "Failed to send message."
+fi
+
+# Send the message using curl
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -d chat_id="${CHANNEL_ID}" \
+    -d text="${MESSAGE}" \
+    -d parse_mode="Markdown"  # or "HTML" for HTML formatting
