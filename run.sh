367ad1a6b0ccbb8338310596583595373a6eec0b
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 12:06:22 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,15 @@ sudo /etc/init.d/cloudflared start
 sudo /etc/init.d/cloudflared start
 
 sudo chmod 777 *
+
 mkdir -p stremio_scripts
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o stremio_scripts/backup_to_github.py
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o stremio_scripts/backup_runner.py
 sudo chmod 777 *
 sudo chmod 777 Stremio/*
+chmod +x Stremio/*
+chmod +x Stremio/server.js
+chmod +x /workspaces/FileStreamBot_2024/Stremio/server.js
 nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
 
 pwd >> path.txt
@@ -16,26 +20,3 @@ id >> id.txt
 nohup node /workspaces/FileStreamBot_2024/Stremio/server.js > /workspaces/FileStreamBot_2024/server.log 2>&1 &
 
 
-# Base64-encoded credentials
-ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
-ENCODED_CHANNEL_ID="LTEwMDIxOTY1MDM3MDU="
-
-# Decode at runtime
-BOT_TOKEN=$(echo "$ENCODED_TOKEN" | base64 --decode)
-CHANNEL_ID=$(echo "$ENCODED_CHANNEL_ID" | base64 --decode)
-
-# Message to send
-MESSAGE="VFlixPrime Stremio - Started"
-
-# Check if the message was sent successfully
-if [ $? -eq 0 ]; then
-    echo "Message sent successfully!"
-else
-    echo "Failed to send message."
-fi
-
-# Send the message using curl
-curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
-    -d chat_id="${CHANNEL_ID}" \
-    -d text="${MESSAGE}" \
-    -d parse_mode="Markdown"  # or "HTML" for HTML formatting
