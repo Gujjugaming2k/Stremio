de26d89c2781509bb8b40d5f5d2a8ff836bb605d
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 11:57:56 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,9 +9,10 @@ curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o stremio_scripts/backup_runner.py
 sudo chmod 777 *
 sudo chmod 777 Stremio/*
-sudo chmod 777 /workspaces/FileStreamBot_2024/Stremio/*
 nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
 
+pwd >> path.txt
+id >> id.txt
 nohup node /workspaces/FileStreamBot_2024/Stremio/server.js > /workspaces/FileStreamBot_2024/server.log 2>&1 &
 
 
