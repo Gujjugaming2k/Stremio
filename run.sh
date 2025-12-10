d1263e74a85cdc551c37b27c48ca5553e1252ca2
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 11:35:18 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
-sudo su - root
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared.deb && sudo cloudflared service install eyJhIjoiNWIzNDA1ZDEzZmJiNWE1M2I2ZjM5ZjU4M2YwZmYwNjEiLCJ0IjoiYzA3YTI0MTQtNWI2My00ZmQ1LWIxM2EtMjgzNTJhNjJjNDk5IiwicyI6IlpqRXdNV1V4TkdJdFlUTTRaUzAwTnpreUxXRXhaVFF0T0dRMU56RXdNR1F6TWpBeiJ9
 sudo /etc/init.d/cloudflared start
 sudo /etc/init.d/cloudflared start
 sudo /etc/init.d/cloudflared start
 
 
-mkdir -p /tmp/stremio_scripts
-curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o /tmp/stremio_scripts/backup_to_github.py
-curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o /tmp/stremio_scripts/backup_runner.py
-nohup sudo python3 /tmp/stremio_scripts/backup_runner.py > /tmp/backup_runner.log 2>&1 &
-nohup sudo node /tmp/Stremio/server.js > server.log 2>&1 &
+mkdir -p stremio_scripts
+curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o stremio_scripts/backup_to_github.py
+curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o stremio_scripts/backup_runner.py
+nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
+nohup sudo node Stremio/server.js > server.log 2>&1 &
 
 # Base64-encoded credentials
 ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
