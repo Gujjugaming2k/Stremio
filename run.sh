b078529236c8f399521d2b0dee4da47042af1261
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 12:51:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,16 +10,18 @@ sudo chmod 777 *
 mkdir -p stremio_scripts
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o stremio_scripts/backup_to_github.py
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o stremio_scripts/backup_runner.py
-sudo chmod 777 *
-sudo chmod 777 Stremio/*
-chmod +x Stremio/*
-chmod +x Stremio/server.js
-chmod +x /workspaces/FileStreamBot_2024/Stremio/server.js
+
 nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
 
 pwd >> path.txt
 id >> id.txt
 sleep 20
+
+sudo chmod 777 *
+sudo chmod 777 Stremio/*
+chmod +x Stremio/*
+chmod +x Stremio/server.js
+chmod +x /workspaces/FileStreamBot_2024/Stremio/server.js
 nohup sudo node /workspaces/FileStreamBot_2024/Stremio/server.js > /workspaces/FileStreamBot_2024/server.log 2>&1 &
 
 # Base64-encoded credentials
