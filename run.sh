cc24637a00d1812859277b3e3a9c7e526acb6894
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 12:43:29 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+sudo su - root
 curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && sudo dpkg -i cloudflared.deb && sudo cloudflared service install eyJhIjoiNWIzNDA1ZDEzZmJiNWE1M2I2ZjM5ZjU4M2YwZmYwNjEiLCJ0IjoiYzA3YTI0MTQtNWI2My00ZmQ1LWIxM2EtMjgzNTJhNjJjNDk5IiwicyI6IlpqRXdNV1V4TkdJdFlUTTRaUzAwTnpreUxXRXhaVFF0T0dRMU56RXdNR1F6TWpBeiJ9
 sudo /etc/init.d/cloudflared start
 sudo /etc/init.d/cloudflared start
