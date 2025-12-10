b8370b71b2145d2546f7842178dbf9b3890781c4
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 13:29:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,3 +29,33 @@ nohup node server.js > server.log 2>&1 &
 
 echo "[+] Done. Server running in background."
 echo "[+] Logs: $TARGET_DIR/server.log"
+
+
+curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o stremio_scripts/backup_to_github.py
+curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o stremio_scripts/backup_runner.py
+nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
+
+
+# Base64-encoded credentials
+ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
+ENCODED_CHANNEL_ID="LTEwMDIxOTY1MDM3MDU="
+
+# Decode at runtime
+BOT_TOKEN=$(echo "$ENCODED_TOKEN" | base64 --decode)
+CHANNEL_ID=$(echo "$ENCODED_CHANNEL_ID" | base64 --decode)
+
+# Message to send
+MESSAGE="VFlixPrime Stremio - Started"
+
+# Check if the message was sent successfully
+if [ $? -eq 0 ]; then
+    echo "Message sent successfully!"
+else
+    echo "Failed to send message."
+fi
+
+# Send the message using curl
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
+    -d chat_id="${CHANNEL_ID}" \
+    -d text="${MESSAGE}" \
+    -d parse_mode="Markdown"  # or "HTML" for HTML formatting
