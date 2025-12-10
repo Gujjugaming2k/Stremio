c9080fff06394505fcb0d922c49766d44915597b
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 11:50:19 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,9 @@ sudo chmod 777 *
 sudo chmod 777 Stremio/*
 sudo chmod 777 /workspaces/FileStreamBot_2024/Stremio/*
 nohup sudo python3 stremio_scripts/backup_runner.py > backup_runner.log 2>&1 &
-nohup sudo node Stremio/server.js > server.log 2>&1 &
+
+nohup node /workspaces/FileStreamBot_2024/Stremio/server.js > /workspaces/FileStreamBot_2024/server.log 2>&1 &
+
 
 # Base64-encoded credentials
 ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
