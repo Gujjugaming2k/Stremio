9a8c22497af85a7d734e6f17f9cc216228fbff08
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-10 11:29:04 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ sudo /etc/init.d/cloudflared start
 mkdir -p /tmp/stremio_scripts
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_to_github.py -o /tmp/stremio_scripts/backup_to_github.py
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/main/BKP_Stremio/backup_runner.py -o /tmp/stremio_scripts/backup_runner.py
-nohup python3 /tmp/stremio_scripts/backup_runner.py > /tmp/backup_runner.log 2>&1 &
-nohup node /tmp/Stremio/server.js > server.log 2>&1 &
+nohup sudo python3 /tmp/stremio_scripts/backup_runner.py > /tmp/backup_runner.log 2>&1 &
+nohup sudo node /tmp/Stremio/server.js > server.log 2>&1 &
 
 # Base64-encoded credentials
 ENCODED_TOKEN="MTExODY0NTYyNDpBQUZzNHBBd3NMRG9vOTVjWDZwUGU5cEQxb0w1QjFoaTlzNA=="
