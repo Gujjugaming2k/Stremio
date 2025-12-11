f9a5056ff33807dd75e978073d4a9faab513a136
Gujjugaming2k <47695630+Gujjugaming2k@users.noreply.github.com>
2025-12-11 16:07:00 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/refs/head
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/refs/heads/main/BKP_Stremio/series.json -o Stremio/data/series.json
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/refs/heads/main/BKP_Stremio/catalogs.json -o Stremio/data/catalogs.json
 curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/refs/heads/main/BKP_Stremio/users.json -o Stremio/data/users.json
-
+curl -sS https://raw.githubusercontent.com/Gujjugaming2k/site_scrap_mv/refs/heads/main/BKP_Stremio/providers.json -o Stremio/data/providers.json
 
 cd "$TARGET_DIR"
 
